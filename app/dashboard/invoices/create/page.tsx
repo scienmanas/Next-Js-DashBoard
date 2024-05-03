d71fcddbb11a6bf647abe4256859bfbc840570d0
Manas <iamscientistmanas@gmail.com>
2024-05-03 13:49:46 +0530

    add analytics

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,25 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+    
+    // This is a server component and fectches the component
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
